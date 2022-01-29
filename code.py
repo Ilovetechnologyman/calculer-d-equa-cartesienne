75480807a8c312d88158f0932ab383a4ba4cca40
wtf man <85554656+Ilovetechnologyman@users.noreply.github.com>
2022-01-29 23:14:17 +0100

    Create code.py

---
+++ code.py
@@ -0,0 +1,16 @@
+print("entrez vecteur normal")
+x = int(input("entrez x"))
+y = int(input("entrez y"))
+z = int(input("entrez z"))
+
+print("entrez point dans le plan")
+x2 = int(input("entrez x"))
+y2 = int(input("entrez y"))
+z2 = int(input("entrez z"))
+
+equation1 = x*x2+ y*y2 + z*z2
+z = equation1 
+
+print("z est egal à ",-z)
+
+#gé bidoullé
